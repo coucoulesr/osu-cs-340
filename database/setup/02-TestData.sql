1d1f66f47a5ed9b0be406cfafcf7b2a7c1ce3dc9
Richard Coucoules <richardcoucoules@gmail.com>
2021-08-09 12:12:59 -0700

    Remove Students_Assignments test data

--- database/setup/02-TestData.sql
+++ database/setup/02-TestData.sql
@@ -40,31 +40,6 @@ VALUES
     ((SELECT id FROM Students WHERE pin=7), (SELECT id FROM Classes WHERE section=10076)),
     ((SELECT id FROM Students WHERE pin=8), (SELECT id FROM Classes WHERE section=13898));
 
-INSERT INTO Students_Assignments
-VALUES
-    ((SELECT id FROM Students WHERE pin=1), (SELECT id FROM Assignments WHERE title="Python Hello World")),
-    ((SELECT id FROM Students WHERE pin=1), (SELECT id FROM Assignments WHERE title="For and While Loops")),
-    ((SELECT id FROM Students WHERE pin=2), (SELECT id FROM Assignments WHERE title="Python Hello World")),
-    ((SELECT id FROM Students WHERE pin=2), (SELECT id FROM Assignments WHERE title="For and While Loops")),
-    ((SELECT id FROM Students WHERE pin=5), (SELECT id FROM Assignments WHERE title="Python Hello World")),
-    ((SELECT id FROM Students WHERE pin=5), (SELECT id FROM Assignments WHERE title="For and While Loops")),
-    ((SELECT id FROM Students WHERE pin=8), (SELECT id FROM Assignments WHERE title="Python Hello World")),
-    ((SELECT id FROM Students WHERE pin=8), (SELECT id FROM Assignments WHERE title="For and While Loops")),
-
-    ((SELECT id FROM Students WHERE pin=2), (SELECT id FROM Assignments WHERE title="Stereochemistry in Benzene Rings")),
-    ((SELECT id FROM Students WHERE pin=2), (SELECT id FROM Assignments WHERE title="SN1 and SN2 Reactions")),
-    ((SELECT id FROM Students WHERE pin=3), (SELECT id FROM Assignments WHERE title="Stereochemistry in Benzene Rings")),
-    ((SELECT id FROM Students WHERE pin=3), (SELECT id FROM Assignments WHERE title="SN1 and SN2 Reactions")),
-    ((SELECT id FROM Students WHERE pin=6), (SELECT id FROM Assignments WHERE title="Stereochemistry in Benzene Rings")),
-    ((SELECT id FROM Students WHERE pin=6), (SELECT id FROM Assignments WHERE title="SN1 and SN2 Reactions")),
-
-    ((SELECT id FROM Students WHERE pin=1), (SELECT id FROM Assignments WHERE title="Impulse-Momentum Theorem")),
-    ((SELECT id FROM Students WHERE pin=1), (SELECT id FROM Assignments WHERE title="Newton's Third Law: Normal Forces")),
-    ((SELECT id FROM Students WHERE pin=4), (SELECT id FROM Assignments WHERE title="Impulse-Momentum Theorem")),
-    ((SELECT id FROM Students WHERE pin=4), (SELECT id FROM Assignments WHERE title="Newton's Third Law: Normal Forces")),
-    ((SELECT id FROM Students WHERE pin=7), (SELECT id FROM Assignments WHERE title="Impulse-Momentum Theorem")),
-    ((SELECT id FROM Students WHERE pin=7), (SELECT id FROM Assignments WHERE title="Newton's Third Law: Normal Forces"));
-
 INSERT INTO Ratings (score, category, assignment_id, author_id)
 VALUES
     (1, "difficulty", (SELECT id FROM Assignments WHERE title="Python Hello World"), (SELECT id FROM Students WHERE pin=1)),
